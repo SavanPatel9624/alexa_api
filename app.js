f8eaafb02f59a06126ece98f5fe94064dc79626a
dev <dev@devs-MacBook-Air.local>
2022-01-07 14:14:56 +0530

    update Package json

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ app.set('view engine', 'ejs')
 app.use(cors());
 app.options('*', cors());
 
+//CORS middleware
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 app.use(bodyParser.urlencoded({ extended : true }))
 
 // Routes
